7f64298a63fff31374f3c08bf3346545be9ecd46
LastAutumnLeaf2000 <pinunileshjadhav2000@gmail.com>
2024-01-10 06:46:05 +0530

    TOMATO

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,23 @@
 import { AllData } from "./utils/AllData";
 import Card from "./Card";
+import { useState } from "react";
 
 const Body = () => {
+  const [resData, setresData] = useState(AllData);
+
   return (
     <div className="body">
-      <h3>Search</h3>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            setresData(AllData.filter((data) => data.info.rating.rating_text >= 4));
+          }}>
+          <h3>Top Rated Restaurants above 4<i class="fa-solid fa-star"></i></h3>
+        </button>
+      </div>
       <div className="card-container">
-        {AllData.map((restaurant) => (
+        {resData.map((restaurant) => (
           <Card key={restaurant.info.resId} resdata={restaurant} />
         ))}
       </div>
